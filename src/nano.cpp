619424d585dc8aa72ad7b748fa1acee4dda8fc8e
{youssef20031} <{youssef.maged.6000@gmail.com}>
2024-12-11 17:25:36 +0200

    ss

--- src/nano.cpp
+++ src/nano.cpp
@@ -16,6 +16,10 @@
 #define MOTOR_ENABLE_PIN 21
 #define MOTOR_IN1_PIN 20
 #define MOTOR_IN2_PIN 19
+// Second motor GPIO pins
+#define MOTOR2_ENABLE_PIN 26
+#define MOTOR2_IN1_PIN 27
+#define MOTOR2_IN2_PIN 28
 #define SERVO_PIN 5
 #define IR_SENSOR_PIN 6
 #define MICROPHONE_PIN 25
@@ -28,25 +32,32 @@ static TaskHandle_t xMicrophoneTaskHandle = NULL;
 static QueueHandle_t xCommandQueue = NULL;
 
 // Command types
-typedef enum {
+typedef enum
+{
     CMD_PAUSE,
     CMD_RESUME
 } Command_t;
 
 // Device instances
 static L298NMotor *motor;
+static L298NMotor *motor2;
 static MG90SServo *servo;
 static DigitalMicrophone *mic;
 
 // Task for motor control and sensor readings
-void vControlTask(void *pvParameters) {
-    while(1) {
+void vControlTask(void *pvParameters)
+{
+    while (1)
+    {
         Command_t cmd;
-        
+
         // Check for commands
-        if(xQueueReceive(xCommandQueue, &cmd, 0) == pdTRUE) {
-            if(cmd == CMD_PAUSE) {
+        if (xQueueReceive(xCommandQueue, &cmd, 0) == pdTRUE)
+        {
+            if (cmd == CMD_PAUSE)
+            {
                 motor->stop();
+                motor2->stop();
                 servo->setPosition(1500);
                 continue;
             }
@@ -57,16 +68,72 @@ void vControlTask(void *pvParameters) {
         int ir_value = infrared_sensor_read();
         printf("Distance: %u cm, IR: %d\n", distance, ir_value);
 
-        if (distance < 20) {
-            motor->stop();
-            if (ir_value) {
-                servo->setPosition(2500);
-                printf("Turning left\n");
-            } else {
-                servo->setPosition(0);
-                printf("Turning right\n");
+        if (distance < 20)
+        {
+            // Stop the motors while waiting
+            motor2->stop();
+
+            int time = 0;
+
+            printf("Moving the second Conveyor belt motor\n");
+
+            if (distance < 10)
+            {
+                // move motor2 for .5 seconds
+                motor2->enable();
+                motor2->setDirection(true);
+                time = 500;
+                vTaskDelay(pdMS_TO_TICKS(time));
+                motor2->stop();
             }
-        } else {
+            else
+            {
+                // move motor2 for 1 seconds
+                motor2->enable();
+                motor2->setDirection(true);
+                time = 1000;
+                vTaskDelay(pdMS_TO_TICKS(time));
+                motor2->stop();
+            }
+            
+            printf("Distance less than 20cm, waiting for IR sensor...\n");
+
+            // Wait for the IR sensor to turn on
+            while (!infrared_sensor_read())
+            {
+                vTaskDelay(pdMS_TO_TICKS(100)); // Delay to prevent busy-waiting
+            }
+
+            // IR sensor is on, turn the servo left
+            servo->setPosition(2500);
+            printf("Turning left\n");
+
+            // Optionally wait before resuming normal operation
+            vTaskDelay(pdMS_TO_TICKS(500));
+
+            if (time == 500)
+            {
+                // move motor2 for .5 seconds
+                motor2->enable();
+                motor2->setDirection(false);
+                vTaskDelay(pdMS_TO_TICKS(time));
+                motor2->stop();
+            }
+            else if (time == 1000)
+            {
+                // move motor2 for .05 seconds
+                motor2->enable();
+                motor2->setDirection(false);
+                vTaskDelay(pdMS_TO_TICKS(time));
+                motor2->stop();
+            }
+
+            // Return to normal operation
+            servo->setPosition(1500);
+            motor->enable();
+        }
+        else
+        {
             motor->enable();
             servo->setPosition(1500);
         }
@@ -76,19 +143,23 @@ void vControlTask(void *pvParameters) {
 }
 
 // Task for microphone monitoring
-void vMicrophoneTask(void *pvParameters) {
+void vMicrophoneTask(void *pvParameters)
+{
     Command_t cmd;
-    while(1) {
-        if(mic->read()) {
+    while (1)
+    {
+        if (mic->read())
+        {
             cmd = CMD_PAUSE;
             xQueueSend(xCommandQueue, &cmd, 0);
             printf("System paused by microphone.\n");
-            
+
             // Wait for sound to stop
-            while(mic->read()) {
+            while (mic->read())
+            {
                 vTaskDelay(pdMS_TO_TICKS(10));
             }
-            
+
             cmd = CMD_RESUME;
             xQueueSend(xCommandQueue, &cmd, 0);
             printf("System resumed.\n");
@@ -97,7 +168,8 @@ void vMicrophoneTask(void *pvParameters) {
     }
 }
 
-int main() {
+int main()
+{
     stdio_init_all();
 
     // Create command queue
@@ -105,16 +177,19 @@ int main() {
 
     // Initialize devices
     motor = new L298NMotor(MOTOR_ENABLE_PIN, MOTOR_IN1_PIN, MOTOR_IN2_PIN);
+    motor2 = new L298NMotor(MOTOR2_ENABLE_PIN, MOTOR2_IN1_PIN, MOTOR2_IN2_PIN);
     servo = new MG90SServo(SERVO_PIN);
     mic = new DigitalMicrophone(MICROPHONE_PIN);
-    
+
     Ultrasonic_Init();
     infrared_sensor_init();
 
     // Set initial motor state
     motor->setDirection(true);
     motor->enable();
-
+    motor2->setDirection(true);
+    motor2->disable();
+    servo->setPosition(1500);
     // Create tasks
     xTaskCreate(
         vControlTask,
@@ -122,8 +197,7 @@ int main() {
         configMINIMAL_STACK_SIZE,
         NULL,
         2,
-        &xControlTaskHandle
-    );
+        &xControlTaskHandle);
 
     xTaskCreate(
         vMicrophoneTask,
@@ -131,13 +205,13 @@ int main() {
         configMINIMAL_STACK_SIZE,
         NULL,
         3,
-        &xMicrophoneTaskHandle
-    );
+        &xMicrophoneTaskHandle);
 
     // Start the scheduler
     vTaskStartScheduler();
 
     // Should never get here
-    while(1);
+    while (1)
+        ;
     return 0;
 }
